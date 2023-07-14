06cc367bcda02e51d69218c15081254b75fd2fc9
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2023-07-13 21:38:05 -0600

    Add modelo de usuario

---
+++ app/Models/Usuario.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class Usuario extends Model implements Authenticatable
+{
+    use HasFactory;
+    protected $fillable=['name','username','password','user_type','remember_token'];
+    protected $table="users";
+
+    // Métodos requeridos por la interfaz Authenticatable
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
