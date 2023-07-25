67709bee2bd47fa2486a5a8d9760fdca515f0f22
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2023-07-24 19:35:48 -0600

    Add logica de  api empleados

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empleado;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $empleados = Empleado::all();
+        return $empleados;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $empleado = new Empleado();
+        $empleado->name = $request->name;
+        $empleado->username = $request->username;
+        $empleado->password = $request->password;
+        $empleado->user_type = $request->user_type;
+
+        $empleado->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $empleado = Empleado::findDrFail($request->id);
+        $empleado->name = $request->name;
+        $empleado->username = $request->username;
+        $empleado->password = $request->password;
+        $empleado->user_type = $request->user_type;
+
+        $empleado->save();
+
+        return $empleado;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Resquest $request)
+    {
+        $empleado = Empleado::destroy(request->id);
+        return $empleado;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/empleados', 'App\Http\Controllers\EmpleadoController@index');
+Route::post('/empleados', 'App\Http\Controllers\EmpleadoController@store');
+Route::put('/empleados/{id}', 'App\Http\Controllers\EmpleadoController@update');
+Route::delete('/empleados/{id}', 'App\Http\Controllers\EmpleadoController@delete');
