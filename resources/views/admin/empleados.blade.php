2d3dbfce0f63b3979f7b6091b2b0a68c796eb842
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2023-07-24 19:35:07 -0600

    Add Interfaz empleados

---
+++ resources/views/admin/empleados.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.plantilla')
+@section('titulos')
+    Empleados
+@endsection
+@section('menuempleado')
+    active
+@endsection
+@section('contenido')
+    <h1>Hola Empleados</h1>
+@endsection
