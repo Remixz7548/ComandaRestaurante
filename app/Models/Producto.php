c00ba7c788293a1a84cfadc18d240499d6db870f
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2023-08-06 18:52:44 -0600

    Add migracion y modelo de tabla productos

---
+++ app/Models/Producto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['name','price','category','status'];
+    protected $table="productos";
+}
